078df150d6d400086d69bdd90f413051d9879722
Komal-8080 <shindeg163@gmail.com>
2020-10-22 17:18:50 +0530

    Coin Flipped Multipal times

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,13 +1,22 @@
 #!/bin/bash -x
 
-heads=1
+heads=0
 tails=0
+numberOfFlips=0
 
-flip=$((RANDOM%2))
-
-if [ $flip -eq $heads ];
+while [ $numberOfFlips -le 10 ]
+do
+	flip=$((RANDOM%2))
+if [ $flip -eq 1 ];
 then
-        echo "!! Heads Win !!"
+        #echo "!! Heads Win !!"
+	heads=$((heads+1))
 else
-        echo "!! Tails Win !!"
+        #echo "!! Tails Win !!"
+	tails=$((tails+1))
 fi
+((numberOfFlips++))
+done
+
+	echo "Heads Win $heads Times"
+	echo "Tails Win $tails Times"
